719d9b8433d21de0af6cba4ad16ab29ba176d9e2
Monty Lennie <montylennie@gmail.com>
2013-03-14 12:17:53 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:show, :edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
@@ -20,11 +27,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -34,10 +39,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
   private
 
   def signed_in_user
-    redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end 
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user) || current_user.admin?
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
 end
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,12 +6,15 @@ describe "User pages" do
 
   describe "profile page" do 
   	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
+    
+  	before do
+      sign_in user 
+      visit user_path(user) 
+    end
 
   	it { should have_selector('h1',    text: user.name) }
   	it { should have_selector('title', text: user.name) }
   end
-
 
   describe "signup page" do
   	before { visit signup_path }
@@ -56,6 +59,51 @@ describe "User pages" do
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign out') }
+      end
+    end
+  end
+
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user) }
+    before(:all)  { 30.times { FactoryGirl.create(:user) } }
+    after(:all)   { User.delete_all }
+
+    before do 
+      sign_in user 
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1', text: 'All users') }
+
+    describe "pagination" do 
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do 
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    describe "delete links" do 
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do 
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do 
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do 
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
   end
